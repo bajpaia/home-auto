4af743d11057ff4a459d2c886b129ee3282ad5cd
bajpaia <animeshbajpai_sanjeev@yahoo.co.in>
2021-07-06 19:58:44 +0530

    update

--- client.py
+++ client.py
@@ -27,7 +27,9 @@ while not connected:
     else:
         connected = True
         print('connected')
-        sio.emit('connection_ack', room.__dict__)
+        room_dict = room
+        room_dict['relays'] = [relay.__dict__ for relay in room_dict['relays']]
+        sio.emit('connection_ack', room_dict)
 
 
 @sio.on('connect')
