ec48a43ecbd3ab863590b52b9a86e525380c2c6c
bajpaia <animeshbajpai_sanjeev@yahoo.co.in>
2021-07-10 22:27:38 +0530

    test2

--- test.py
+++ test.py
@@ -7,16 +7,16 @@ servo_vertical = ServoMotor(1, 1500)
 if __name__ == '__main__':
     
 
-    # for i in range(500, 2580, 11):
-    #     servo_horizontal.move_by_pulse(i)
-    #     servo_vertical.move_by_pulse(i)
-    # servo_horizontal.move_by_pulse(1500)
-    # time.sleep(0.01)
-    # servo_vertical.move_by_pulse(1500)    
-
-    for i in range(0, 180, 2):
-        servo_horizontal.move_to_degree(i)
-        servo_vertical.move_to_degree(i)
-    servo_horizontal.move_to_degree(90)
+    for i in range(500, 2580, 11):
+        servo_horizontal.move_by_pulse(i)
+        servo_vertical.move_by_pulse(i)
+    servo_horizontal.move_by_pulse(1500)
     time.sleep(0.01)
-    servo_vertical.move_to_degree(90)     
+    servo_vertical.move_by_pulse(1500)    
+
+    # for i in range(0, 180, 2):
+    #     servo_horizontal.move_to_degree(i)
+    #     servo_vertical.move_to_degree(i)
+    # servo_horizontal.move_to_degree(90)
+    # time.sleep(0.01)
+    # servo_vertical.move_to_degree(90)     
