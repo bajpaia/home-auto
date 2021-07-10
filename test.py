14351d0dec22391e9ef6d8e6b38ad513228dd934
bajpaia <animeshbajpai_sanjeev@yahoo.co.in>
2021-07-10 16:25:16 +0530

    test move by degree

--- test.py
+++ test.py
@@ -7,9 +7,16 @@ servo_vertical = ServoMotor(1)
 if __name__ == '__main__':
     
 
-    for i in range(500, 2580, 11):
-        servo_horizontal.move_by_pulse(i)
-        servo_vertical.move_by_pulse(i)
-    servo_horizontal.move_by_pulse(1500)
+    # for i in range(500, 2580, 11):
+    #     servo_horizontal.move_by_pulse(i)
+    #     servo_vertical.move_by_pulse(i)
+    # servo_horizontal.move_by_pulse(1500)
+    # time.sleep(0.01)
+    # servo_vertical.move_by_pulse(1500)    
+
+    for i in range(0, 180, 2.5):
+        servo_horizontal.move_to_degree(i)
+        servo_vertical.move_to_degree(i)
+    servo_horizontal.move_to_degree(90)
     time.sleep(0.01)
-    servo_vertical.move_by_pulse(1500)      
+    servo_vertical.move_to_degree(90)     
