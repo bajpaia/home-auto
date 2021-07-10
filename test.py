d92d548431aa66739e33ff612d75ec1a2d6671d4
bajpaia <animeshbajpai_sanjeev@yahoo.co.in>
2021-07-10 16:35:38 +0530

    tst

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 from models import ServoMotor
 import time
 
-servo_horizontal = ServoMotor(init_pulse = 2500)
-servo_vertical = ServoMotor(1, 500)
+servo_horizontal = ServoMotor(init_pulse = 1500)
+servo_vertical = ServoMotor(1, 1500)
 
 if __name__ == '__main__':
     
@@ -17,6 +17,6 @@ if __name__ == '__main__':
     for i in range(0, 180, 2):
         servo_horizontal.move_to_degree(i)
         servo_vertical.move_to_degree(i)
-    servo_horizontal.move_to_degree(0)
+    servo_horizontal.move_to_degree(90)
     time.sleep(0.01)
     servo_vertical.move_to_degree(90)     
