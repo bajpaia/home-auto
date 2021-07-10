6dc4ab131ca6971b27c0f3d50cecfed24f209536
bajpaia <animeshbajpai_sanjeev@yahoo.co.in>
2021-07-10 23:04:15 +0530

    testing move_by degree

--- test.py
+++ test.py
@@ -6,17 +6,17 @@ servo_horizontal = 0
 servo_vertical = 1
 
 if __name__ == '__main__':
-    for i in range(500, 2580, 11):
-        driver.move_by_pulse(servo_horizontal,i)
-        driver.move_by_pulse(servo_vertical,i)
+    # for i in range(500, 2580, 11):
+    #     driver.move_by_pulse(servo_horizontal,i)
+    #     driver.move_by_pulse(servo_vertical,i)
 
-    driver.move_by_pulse(servo_horizontal, 1500)
-    time.sleep(0.20)
-    driver.move_by_pulse(servo_vertical, 1500)    
+    # driver.move_by_pulse(servo_horizontal, 1500)
+    # time.sleep(0.20)
+    # driver.move_by_pulse(servo_vertical, 1500)    
 
-    # for i in range(0, 180, 2):
-    #     servo_horizontal.move_to_degree(i)
-    #     servo_vertical.move_to_degree(i)
-    # servo_horizontal.move_to_degree(90)
-    # time.sleep(0.01)
-    # servo_vertical.move_to_degree(90)     
+    for i in range(0, 180, 2):
+        servo_horizontal.move_to_degree(i)
+        servo_vertical.move_to_degree(i)
+    servo_horizontal.move_to_degree(90)
+    time.sleep(0.01)
+    servo_vertical.move_to_degree(90)     
