8ea01f870adc78b6d74f924c68ea954686caff92
bajpaia <animeshbajpai_sanjeev@yahoo.co.in>
2021-07-10 22:50:27 +0530

    testing demo

--- PCA9685.py
+++ PCA9685.py
@@ -85,12 +85,18 @@ if __name__=='__main__':
  
   pwm = PCA9685(0x40, debug=False)
   pwm.setPWMFreq(50)
+  
+
   while True:
    # setServoPulse(2,2500)
     for i in range(500,2500,10):  
       pwm.setServoPulse(0,i)   
-      time.sleep(0.02)     
+      time.sleep(0.1) 
+      pwm.setServoPulse(1,i)   
+      time.sleep(0.1)      
     
     for i in range(2500,500,-10):
       pwm.setServoPulse(0,i) 
-      time.sleep(0.02)  
+      time.sleep(0.1)  
+      pwm.setServoPulse(1,i)   
+      time.sleep(0.1)
