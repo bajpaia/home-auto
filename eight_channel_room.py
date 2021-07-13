8159794ad908d792c073baf77ecee965170e9ed0
bajpaia <animeshbajpai_sanjeev@yahoo.co.in>
2021-07-13 19:45:08 +0530

    test

--- eight_channel_room.py
+++ eight_channel_room.py
@@ -70,9 +70,11 @@ def execute(data):
 
 @sio.on('change_relay_names')
 def change_name_relay(data):
+    print(data)
     for relay in room.relays:
         if data['pin']==relay.pin and len(data['name'])>0:
             relay.name = data['name']
+
     room.save()
     room_json = get_room()
     sio.emit('connection_ack', room_json)
